6eb9ba1ddb9b73cacf4109a179a8d857204403fe
lekapinun <lekapinun@gmail.com>
2017-06-14 17:11:52 +0700

    update Login

--- blood-now/screens/LoginScreen.js
+++ blood-now/screens/LoginScreen.js
@@ -1,36 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
   Text,
   View,
   TouchableOpacity,
   TextInput,
+  Button,
 } from 'react-native';
 
 export default class LoginScreen extends Component {
 
     state = { 
-        email: '',
+        id: '',
         password: '',
-        error: '',
     };
 
     render() {
         return(
             <View style={{marginTop:30}}>
                 <Text>LOGIN SCREEN</Text>
-                <View style={{marginTop:30}}>
-                    <TouchableOpacity onPress={this._loginSuccess}>
-                        <Text style={{fontSize:20, color: '#EF685E'}}>
-                            LOGIN BUTTON
-                        </Text>
-                    </TouchableOpacity>
-                    <View style={{height:50,backgroundColor:'#EF685E'}}></View>
+                <View>
+                    <TextInput
+                        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                        autoCorrect={false}
+                        onChangeText={(id) => this.setState({id})}
+                        value={this.state.id}
+                        placeholder="ชื่อผู้ใช้หรือเบอร์โทรศัพท์"
+                    />
+                    <TextInput
+                        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                        autoCorrect={false}
+                        onChangeText={(password) => this.setState({password})}
+                        value={this.state.password}
+                        placeholder="รหัสผ่าน"
+                    />
                 </View>
+                <View style={{marginTop:30,backgroundColor: '#EF685E'}}>
+                    <Button title="เข้าสู่ระบบ" onPress={this._loginSuccess} color="white" />   
+                </View>
+                <View style={{marginTop:30,backgroundColor: '#EF685E'}}>
+                    <Button title="ลงทะเบียน" onPress={this._register} color="white" />    
+                </View>
+                
+               
             </View>
         );
     }
 
     _loginSuccess = () => {
         this.props.navigator.push("rootNavigation");
+    };
+
+    _register = () => {
+        this.props.navigator.push("register");
     };
 }
