53a89478208754701f75190420b1b4630daf8b7d
au12113 <wasupont@windowslive.com>
2017-06-23 11:23:42 +0700

    just save

--- blood-now/screens/InformationScreen.js
+++ blood-now/screens/InformationScreen.js
@@ -4,11 +4,31 @@ import {
   View,
   TouchableOpacity,
 } from 'react-native';
+import { SlidingTabNavigation } from 'ex-navigation';
 
 export default class InformationScreen extends Component {
+    static route = {
+      navigationBar: {
+        title="ข้อควรรู้เกี่ยวกับการบริจาคโลหิต"
+      }
+    };
+
     render() {
         return(
             <View style={{marginTop:30}}>
+              <SlidingTabNavigation
+                id="sliding-tab-navigation"
+                navigatorUID="sliding-tab-navigation"
+                initialTab="ขั้นตอน"
+                renderLabel={this._renderLabel}
+                barBackgrundColor="#000000"
+                indicatorStyle={styles.tabIndicator}
+              >
+
+                <SlidingTabNavigationItem id="ขั้นตอน">
+                  <View />
+                </SlidingTabNavigationItem>
+              </SlidingTabNavigation>
                 <Text>INFORMATION SCREEN</Text>
             </View>
         );
