90fa43325b739cf60c1a14b49212be0e27fbcd5b
au12113 <wasupont@windowslive.com>
2017-06-30 05:57:17 +0700

    fixed can't push after login
    use replace instead

--- blood-now/screens/LoginScreen.js
+++ blood-now/screens/LoginScreen.js
@@ -130,8 +130,7 @@ export default class LoginScreen extends Component {
                 userData = JSON.parse(responseText);
                 console.log('login success');
                 this._userData(userData);
-                this.props.navigator.push('rootNavigationSliding');
-                this.props.navigator.pop();
+                this.props.navigator.replace('rootNavigationSliding');
             }
             else
             {
